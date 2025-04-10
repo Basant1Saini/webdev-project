a1f783691fa803a8df08cfca73f9fe36360879da
basant saini <basant357@gmail.com>
2025-04-10 08:18:39 +0530

    refactor index.js; remove unused imports and streamline rendering logic

--- client/src/index.js
+++ client/src/index.js
@@ -24,23 +24,3 @@ root.render(
   </React.StrictMode>
 );
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import store from './store';
-import './styles/index.css';
-import './styles/forms.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
-);
-
