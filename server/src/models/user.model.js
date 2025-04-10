10ac08ca06317266b1db4ea58f8031dded678984
basant saini <basant357@gmail.com>
2025-04-10 08:46:44 +0530

    refactor authentication middleware and user model for improved structure and readability

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -24,68 +24,6 @@ module.exports = (sequelize) => {
     password: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
-    role: {
-      type: DataTypes.ENUM('user', 'admin'),
-      defaultValue: 'user',
-    },
-  }, {
-    hooks: {
-      beforeCreate: async (user) => {
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(user.password, salt);
-      },
-      beforeUpdate: async (user) => {
-        if (user.changed('password')) {
-          const salt = await bcrypt.genSalt(10);
-          user.password = await bcrypt.hash(user.password, salt);
-        }
-      },
-    },
-  });
-
-  // Compare password
-  User.prototype.checkPassword = async function(enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
-  };
-
-  // Generate JWT token
-  User.prototype.generateToken = function() {
-    return jwt.sign(
-      { id: this.id },
-      process.env.JWT_SECRET,
-      { expiresIn: process.env.JWT_EXPIRES_IN || '7d' }
-    );
-  };
-
-  return User;
-};
-
-const { DataTypes } = require('sequelize');
-const bcrypt = require('bcryptjs');
-
-module.exports = (sequelize) => {
-  const User = sequelize.define('User', {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
       validate: {
         len: [6, 100],
       },
@@ -124,6 +62,15 @@ module.exports = (sequelize) => {
   // Instance method to check if password matches
   User.prototype.checkPassword = async function(candidatePassword) {
     return await bcrypt.compare(candidatePassword, this.password);
+  };
+  
+  // Generate JWT token
+  User.prototype.generateToken = function() {
+    return jwt.sign(
+      { id: this.id },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN || '7d' }
+    );
   };
 
   // Add any associations here
